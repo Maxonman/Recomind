cbe4465749a26089c51d35bde95e81d6b1b8d0a9
Elias Valle <91441332+Maxonman@users.noreply.github.com>
2021-12-06 13:42:14 -0300

    Filtros
    Se implemento un inicio del sistema de filtros.

--- RecomindApp/models.py
+++ RecomindApp/models.py
@@ -23,30 +23,6 @@ class Categorias(models.Model):
     
     def __str__(self):
         return self.Nombre
-    
-class Items(models.Model):
-    ID = models.AutoField(primary_key=True)
-    Tipo = models.ForeignKey(Tipos, on_delete=models.CASCADE)
-    Nombre = models.CharField(max_length=100)
-    Imagen = models.CharField(max_length=100, null=True)
-    Descripcion = models.CharField(max_length=1000)
-    Categorias = models.ForeignKey(Categorias, on_delete=models.SET_NULL, null=True)
-    Valoracion = models.FloatField()
-    Year = models.IntegerField()
-    Director = models.ForeignKey(Directores, on_delete=models.SET_NULL, null=True)
-    Terminado = models.BooleanField()
-    Last_Update = models.DateField()
-    Cantidad_Capitulos = models.IntegerField()
-    
-    def __str__(self):
-        return self.Nombre
-    
-
-    
-class Categorias_Aplicadas(models.Model):
-    ID = models.AutoField(primary_key=True)
-    Categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE)
-    Item = models.ForeignKey(Items, on_delete=models.CASCADE)
 
 class Estudio(models.Model):
     ID = models.AutoField(primary_key=True)
@@ -61,3 +37,30 @@ class Desarrollador(models.Model):
     
     def __str__(self):
         return self.Nombre
+    
+class Items(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Tipo = models.ForeignKey(Tipos, on_delete=models.CASCADE)
+    Nombre = models.CharField(max_length=100)
+    Imagen = models.CharField(max_length=100, null=True)
+    Descripcion = models.CharField(max_length=1000)
+    Categorias = models.ForeignKey(Categorias, on_delete=models.SET_NULL, null=True)
+    Valoracion = models.FloatField()
+    Year = models.IntegerField()
+    Director = models.ForeignKey(Directores, on_delete=models.SET_NULL, null=True)
+    Desarrollador = models.ForeignKey(Desarrollador, on_delete=models.SET_NULL, null=True)
+    Estudio = models.ForeignKey(Estudio, on_delete=models.SET_NULL, null=True)
+    Terminado = models.BooleanField()
+    Last_Update = models.DateField()
+    Cantidad_Capitulos = models.IntegerField()
+    
+    def __str__(self):
+        return self.Nombre
+
+
+    
+class Categorias_Aplicadas(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE)
+    Item = models.ForeignKey(Items, on_delete=models.CASCADE)
+
